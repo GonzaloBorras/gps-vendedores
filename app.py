d0978831f85ae4de555252887ba6b86404a3c90a
GonzaloBorras <gon.borras2@gmail.com>
2026-08-04 19:31:40 -0300

    update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ PDV_FILE = os.path.join(BASE_DIR, 'pdv.json')
 RUTAS_FILE = os.path.join(BASE_DIR, 'rutas.json')
 OVERRIDES_FILE = os.path.join(BASE_DIR, 'overrides.json')
 
-APP_VERSION = '2.1'
-APP_VERSION_CODE = 3
+APP_VERSION = '2.2'
+APP_VERSION_CODE = 4
 APK_URL = 'https://github.com/GonzaloBorras/gps-vendedores/releases/download/apk-v1.0/GPS-Merchan.apk'
 APK_URL_ADMIN = 'https://github.com/GonzaloBorras/gps-vendedores/releases/download/apk-admin/GPS-Admin.apk'
 
